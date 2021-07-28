74fbc38d940d393a2f6932ce3ba5ba789e2ace48
Paula Buscacio <paula@buscacio.net>
2021-07-28 20:05:54 -0300

    Projeto criado

---
+++ src/test/java/net/buscacio/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.buscacio.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
