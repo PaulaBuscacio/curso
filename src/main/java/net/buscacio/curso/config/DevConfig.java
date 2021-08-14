c2569d3c392f1ba44b07ec7c1d0294c35dfad54f
Paula Buscacio <paula@buscacio.net>
2021-08-14 16:51:43 -0300

    conexao mysql criada

---
+++ src/main/java/net/buscacio/curso/config/DevConfig.java
@@ -0,0 +1,29 @@
+package net.buscacio.curso.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import net.buscacio.curso.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
