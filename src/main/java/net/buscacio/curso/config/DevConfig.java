c499951d87630e2be7f87b159824fa2c43a4d479
Paula Buscacio <paula@buscacio.net>
2021-08-19 17:56:27 -0300

    Serviço de email Html criado

--- src/main/java/net/buscacio/curso/config/DevConfig.java
+++ src/main/java/net/buscacio/curso/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import net.buscacio.curso.services.DBService;
+import net.buscacio.curso.services.EmailService;
+import net.buscacio.curso.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -24,6 +26,11 @@ public class DevConfig {
 	public boolean instanciateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
